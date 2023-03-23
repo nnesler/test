1b58c3a8fcb02093ecc9585d7bb3a15fe1690d0b
Matthew Mandzen <mmandzen@uiowa.edu>
2023-03-23 12:29:37 -0500

    about page 1

--- app.py
+++ app.py
@@ -44,6 +44,10 @@ def add():
     else:
         return redirect(url_for("index"))
 
+@app.route("/about", methods=["POST"])
+def about():
+    return render_template(
+        "about.html", pageTitle="About Page"
 
 if __name__ == "__main__":
     app.run(debug=True)
